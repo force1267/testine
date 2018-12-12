d248b1c7976ebf837d41fb032161a0908a4d7bb3
root <force1267@gmail.com>
2018-12-12 20:43:45 +0000

    update 2

--- debuggingAPI/index.js
+++ debuggingAPI/index.js
@@ -26,9 +26,9 @@ app.use('/route', route({express, User, Post, Comments}))
 app.listen(5430, 'localhost')
 console.log('Server listening ...')
 
-mongoose.connect('mongodb://' + cfg.MONGO_HOST + ':' + cfg.MONGO_PORT + '/' + cfg.MONGO_DATABASE, {
-user: cfg.MONGO_USER,
-pass: cfg.MONGO_PASS,
+mongoose.connect('mongodb://cluster0-shard-00-00-ph4su.mongodb.net:27017,cluster0-shard-00-01-ph4su.mongodb.net:27017,cluster0-shard-00-02-ph4su.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', {
+user: 'force',
+pass: '12672551',
 useNewUrlParser: true
 })
 
